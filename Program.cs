f6dc0f4a718502ad4ed5ffe8d0bb2f0d9f3e75f0
ragbot <64681480+Ragbot@users.noreply.github.com>
2021-01-08 14:34:24 +0530

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS_Narvel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.length == 1)
+                get(args[1]);
+            Console.WriteLine("Hello World!");
+        }
+
+        void get(string location)
+        {
+            switch (location)
+            {
+                case ".":
+                    get("curren_directory");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
